e65d83033ee240731055760711172251d551ffea
chrisfinco <chrisfinco@yahoo.com>
2021-09-17 02:52:15 -0600

    Defined Assessment Configuration page objects

---
+++ CSET_Selenium/CSET_Selenium/Page_Objects/Assessment_Configuration/Assessment_Configuration.cs
@@ -0,0 +1,41 @@
+﻿using CSET_Selenium.DriverConfiguration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSET_Selenium.Page_Objects.Assessment_Configuration
+{
+    class Assessment_Configuration
+    {
+        [FindsBy(How = How.XPath, Using = "//input[contains(@name,'email')]")]
+        private readonly IWebElement textboxEmail;
+
+        [FindsBy(How = How.XPath, Using = "//input[contains(@name, 'date')]")]
+        private readonly IWebElement textboxDate;
+
+        [FindsBy(How = How.XPath, Using = "//input[contains(@name, 'facility')]")]
+        private readonly IWebElement textboxFacility;
+
+        [FindsBy(How = How.XPath, Using = "//input[contains(@name, 'citySite')]")]
+        private readonly IWebElement textboxCitySite;
+
+        [FindsBy(How = How.XPath, Using = "//input[contains(@name, 'stateProvRegion')]")]
+        private readonly IWebElement textboxStateProvRegion;
+
+        [FindsBy(How = How.XPath, Using = "//input[contains(@name,'maturity')]")]
+        private readonly IWebElement checkboxMaturity;
+
+        [FindsBy(How = How.XPath, Using = "//input[contains(@name,'standard')]")]
+        private readonly IWebElement checkboxStandard;
+
+        [FindsBy(How = How.XPath, Using = "//input[contains(@name,'diagram')]")]
+        private readonly IWebElement checkboxDiagram;
+
+        [FindsBy(How = How.XPath, Using = "//button[contains(text(), 'Next')]")]
+        private readonly IWebElement buttonNext;
+    }
+}
