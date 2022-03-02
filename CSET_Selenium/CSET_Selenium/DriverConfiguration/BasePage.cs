f5396746f334989c70d8424f2247f809da7f534f
hanskp <kyle.hanson@inl.gov>
2022-03-02 16:00:44 -0700

    QC-9

--- CSET_Selenium/CSET_Selenium/DriverConfiguration/BasePage.cs
+++ CSET_Selenium/CSET_Selenium/DriverConfiguration/BasePage.cs
@@ -111,7 +111,7 @@ namespace CSET_Selenium.DriverConfiguration
                     result = true;
                     break;
                 }
-                catch (StaleElementReferenceException e)
+                catch (StaleElementReferenceException e1)
                 {
                 }
                 attempts++;
