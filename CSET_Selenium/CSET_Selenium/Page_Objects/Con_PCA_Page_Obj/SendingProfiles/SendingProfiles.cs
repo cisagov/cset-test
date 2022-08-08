7ac35c5e04eab5c39ad9e5de7d1c57a14258aa96
JessicaQu <hongjie.qu@inl.gov>
2022-08-08 17:22:00 -0600

    Sending profiles

---
+++ CSET_Selenium/CSET_Selenium/Page_Objects/Con_PCA_Page_Obj/SendingProfiles/SendingProfiles.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSET_Selenium.Enums.Con_PCA;
+using OpenQA.Selenium;
+
+namespace CSET_Selenium.Page_Objects.Con_PCA_Page_Obj.SendingProfiles
+{
+    class SendingProfiles : ConPCABase
+    {
+        private readonly IWebDriver driver;
+
+        public SendingProfiles(IWebDriver driver) : base(driver)
+        {
+            this.driver = driver;
+        }
+
+        //Element Locators
+
+        private IWebElement ButtonNewProfile
+        {
+            get
+            {
+                return WaitUntilElementIsVisible(By.XPath("//span[text()=' New Profile ']"));
+            }
+        }
+
+        private IWebElement ButtonSaveProfile
+        {
+            get
+            {
+                return WaitUntilElementIsVisible(By.XPath("//span[text()=' Save Profile ']"));
+            }
+        }
+
+
+        private IWebElement TextboxDomainName
+        {
+            get
+            {
+                return WaitUntilElementIsVisible(By.XPath("//input[@formcontrolname='name']"));
+            }
+        }
+
+        private IWebElement MatSelectInterfaceType
+        {
+            get
+            {
+                return WaitUntilElementIsVisible(By.XPath("//mat-select[@formcontrolname='interfaceType']"));
+            }
+        }
+
+        private IWebElement TableProfiles
+        {
+            get
+            {
+                return WaitUntilElementIsVisible(By.XPath("//mat-table[@class='mat-table cdk-table mat-sort']"));
+            }
+        }
+
+        //Interaction Methods
+        private void ClickNewProfileButton()
+        {
+            ClickWhenClickable(ButtonNewProfile);
+        }
+        
+        private void ClickSaveProfileButton()
+        {
+            ClickWhenClickable(ButtonSaveProfile);
+        }
+
+        private void SelectInterfaceType(String type)
+        {
+            ClickWhenClickable(MatSelectInterfaceType);
+            Find(By.XPath(".//mat-option[span[text() = '" + type + "']]")).Click();
+        }
+
+        private IWebElement GetProfilesTable()
+        {
+            return TableProfiles;
+        }
+
+        //Aggregate Methods
+        public void SetDomainName(String name)
+        {
+            int i = 0;
+            do
+            {
+                TextboxDomainName.SendKeys(Keys.Control + "a");
+                TextboxDomainName.SendKeys(Keys.Delete);
+                i++;
+            } while ((!TextboxDomainName.Text.Equals("")) && i < 3);
+            TextboxDomainName.SendKeys(name);
+        }
+
+        public IList<IWebElement> GetProfilesTableRows()
+        {
+            IList<IWebElement> rows;
+            rows = GetProfilesTable().FindElements(By.TagName("mat-row"));
+            return rows;
+        }
+
+        public void CreateNewProfile(String domainName, String interFaceType)
+        {
+            ClickNewProfileButton();
+            SetDomainName(domainName);
+            SelectInterfaceType(interFaceType);
+            ClickSaveProfileButton();
+            ClickOKFromPopup();
+        }
+    }
+ }
---
+++ CSET_Selenium/CSET_Selenium/Tests/Con_PCA/Sending_Profiles/SendingProfileCreateEditDelete.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CSET_Selenium.DriverConfiguration;
+using CSET_Selenium.ConPCA_Repository.Login_Page;
+using CSET_Selenium.Page_Objects.Con_PCA_Page_Obj.SideMenu;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using CSET_Selenium.Helpers;
+using CSET_Selenium.Enums.Con_PCA;
+using CSET_Selenium.Page_Objects.Con_PCA_Page_Obj.SendingProfiles;
+
+namespace CSET_Selenium.Tests.Con_PCA.Sending_Profiles
+{
+    [TestFixture]
+    public class LandingPageCreateEditDeleteTest : BaseTest
+    {
+        private IWebDriver driver;
+
+        [Test]
+        public void SendingProfileTest()
+        {
+            BaseConfiguration cf = new BaseConfiguration(Env.Dev.GetValue());
+            driver = BuildDriver(cf);
+            String landingPageName = StringsUtils.GenerateRandomString(6)+".xyz";
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.LoginToConPCA(LoginInfo.User_Name.GetValue(), LoginInfo.Password.GetValue());
+
+            SideMenu sideMenu = new SideMenu(driver);
+            sideMenu.SelectSendingProfiles();
+            //Create a new profile
+            SendingProfiles profile = new SendingProfiles(driver);
+
+            Console.WriteLine("");
+
+        }
+    }
+}
